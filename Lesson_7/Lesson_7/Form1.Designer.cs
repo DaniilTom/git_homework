5ae8b12a161d85a7123d96d3afefe6009b10288d
DaniilTom <DaniilTom@users.noreply.github.com>
2018-11-24 17:17:55 +0300

    Lesson_7 begin

---
+++ Lesson_7/Lesson_7/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Lesson_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.labelCommand = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.labelCurrentNum = new System.Windows.Forms.Label();
+            this.labelCommandNum = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отменитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.подчказкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelNumToGuess = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.AccessibleName = "add_1";
+            this.btnCommand1.Location = new System.Drawing.Point(207, 57);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "button1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btns_Action_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.AccessibleName = "mul_2";
+            this.btnCommand2.Location = new System.Drawing.Point(207, 103);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "button2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btns_Action_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.AccessibleName = "reset";
+            this.btnReset.Location = new System.Drawing.Point(207, 144);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "button3";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(105, 83);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(0, 13);
+            this.lblNumber.TabIndex = 3;
+            // 
+            // labelCommand
+            // 
+            this.labelCommand.AutoSize = true;
+            this.labelCommand.Location = new System.Drawing.Point(136, 139);
+            this.labelCommand.Name = "labelCommand";
+            this.labelCommand.Size = new System.Drawing.Size(0, 13);
+            this.labelCommand.TabIndex = 3;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // labelCurrentNum
+            // 
+            this.labelCurrentNum.AutoSize = true;
+            this.labelCurrentNum.Location = new System.Drawing.Point(63, 83);
+            this.labelCurrentNum.Name = "labelCurrentNum";
+            this.labelCurrentNum.Size = new System.Drawing.Size(36, 13);
+            this.labelCurrentNum.TabIndex = 4;
+            this.labelCurrentNum.Text = "Чило:";
+            // 
+            // labelCommandNum
+            // 
+            this.labelCommandNum.AutoSize = true;
+            this.labelCommandNum.Location = new System.Drawing.Point(12, 139);
+            this.labelCommandNum.Name = "labelCommandNum";
+            this.labelCommandNum.Size = new System.Drawing.Size(118, 13);
+            this.labelCommandNum.TabIndex = 4;
+            this.labelCommandNum.Text = "Количество комманд:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(303, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem,
+            this.отменитьToolStripMenuItem,
+            this.подчказкаToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            this.игратьToolStripMenuItem.Click += new System.EventHandler(this.playToolStripMenuItem_Click);
+            // 
+            // отменитьToolStripMenuItem
+            // 
+            this.отменитьToolStripMenuItem.AccessibleName = "back";
+            this.отменитьToolStripMenuItem.Name = "отменитьToolStripMenuItem";
+            this.отменитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.отменитьToolStripMenuItem.Text = "Отменить";
+            this.отменитьToolStripMenuItem.Click += new System.EventHandler(this.backTo);
+            // 
+            // подчказкаToolStripMenuItem
+            // 
+            this.подчказкаToolStripMenuItem.Name = "подчказкаToolStripMenuItem";
+            this.подчказкаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.подчказкаToolStripMenuItem.Text = "Подсказка";
+            this.подчказкаToolStripMenuItem.Click += new System.EventHandler(this.tipToolStripMenuItem_Click);
+            // 
+            // labelNumToGuess
+            // 
+            this.labelNumToGuess.AutoSize = true;
+            this.labelNumToGuess.Location = new System.Drawing.Point(105, 108);
+            this.labelNumToGuess.Name = "labelNumToGuess";
+            this.labelNumToGuess.Size = new System.Drawing.Size(0, 13);
+            this.labelNumToGuess.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Загаданное число:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 203);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelCommandNum);
+            this.Controls.Add(this.labelNumToGuess);
+            this.Controls.Add(this.labelCurrentNum);
+            this.Controls.Add(this.labelCommand);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label labelCommand;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label labelCurrentNum;
+        private System.Windows.Forms.Label labelCommandNum;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отменитьToolStripMenuItem;
+        private System.Windows.Forms.Label labelNumToGuess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem подчказкаToolStripMenuItem;
+    }
+}
+
