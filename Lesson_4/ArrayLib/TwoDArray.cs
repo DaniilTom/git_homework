71dbcff0f12764f36b5e763f02c9124b75e05e5b
DaniilTom <DaniilTom@users.noreply.github.com>
2018-12-06 23:32:52 +0300

    Revert "Revert "Merge branch 'CSharp_Part_1' of https://github.com/DaniilTom/git_homework into CSharp_Part_1""
    This reverts commit f85032bcee068f945652fecd50534e1d2b8c7938.

---
+++ Lesson_4/ArrayLib/TwoDArray.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLib
+{
+    /*
+		Томашевич
+	*а) Реализовать библиотеку с классом для работы с двумерным массивом.
+		Реализовать конструктор, заполняющий массив случайными числами.
+		Создать методы, которые возвращают сумму всех элементов массива,
+		сумму всех элементов массива больше заданного, свойство,
+		возвращающее минимальный элемент массива, свойство, возвращающее максимальный элемент массива, метод, 
+		возвращающий номер максимального элемента массива (через параметры, используя модификатор ref или out).
+	**б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+	**в) Обработать возможные исключительные ситуации при работе с файлами.
+	*/
+
+    public class TwoDArray
+    {
+        int[,] a;
+
+        public TwoDArray(int n, int el)
+        {
+            a = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    a[i, j] = el;
+        }
+        public TwoDArray(int n, int min, int max)
+        {
+            a = new int[n, n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    a[i, j] = rnd.Next(min, max);
+        }
+
+        /// <summary>
+        /// Конструктор для загрузки массива из файла.
+        /// </summary>
+        /// <param name="fileName">Имя файла</param>
+        public TwoDArray(string fileName)
+        {
+            FileStream fstr = null;
+            BinaryReader br = null;
+            try
+            {
+                fstr = new FileStream(fileName, FileMode.Open);
+                br = new BinaryReader(fstr);
+                int size = (int)Math.Sqrt(fstr.Length / sizeof(int));
+
+                a = new int[size, size];
+
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        a[i, j] = br.ReadInt32();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл не найден.");
+                throw;  //чтобы предотвратить создание объекта TwoDArray
+            }
+            finally
+            {
+                br.Close();
+            }
+        }
+
+        /// <summary>
+        /// Метода записи массива в файл.
+        /// </summary>
+        /// <param name="fileName">Имя файла</param>
+        public void WriteInFile(string fileName)
+        {
+            FileStream fstr = new FileStream(fileName, FileMode.OpenOrCreate);
+            BinaryWriter bw = new BinaryWriter(fstr);
+
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    bw.Write(a[i, j]);
+
+            bw.Close();
+        }
+
+        public int Min
+        {
+            get
+            {
+                int min = a[0, 0];
+                // Находим минимальный элемент
+                // В двухмерном массиве для получения размерности нужно использовать
+                // метод GetLength, а в скобках указывать измерение
+                for (int i = 0; i < a.GetLength(0); i++)
+                    for (int j = 0; j < a.GetLength(1); j++)
+                        if (a[i, j] < min) min = a[i, j];
+                return min;
+            }
+        }
+        public int Max
+        {
+            get
+            {
+                int max = a[0, 0];
+                for (int i = 0; i < a.GetLength(0); i++)
+                    for (int j = 0; j < a.GetLength(1); j++)
+                        if (a[i, j] > max) max = a[i, j];
+                return max;
+            }
+        }
+
+        /// <summary>
+        /// Находит позицию первого максимального элемента.
+        /// </summary>
+        /// <param name="row">Строка</param>
+        /// <param name="column">Столбец</param>
+        public void MaxElementPosition(out int row, out int column)
+        {
+            int max = a[0, 0];
+            row = 0;
+            column = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                        row = i;
+                        column = j;
+                    }
+        }
+
+
+        /// <summary>
+        /// Подсчет количества положительных элементов.
+        /// </summary>
+        public int CountPositive
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.GetLength(0); i++)
+                    for (int j = 0; j < a.GetLength(1); j++)
+                        if (a[i, j] > 0) count++;
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// Расчет среднего арифметического.
+        /// </summary>
+        public double Average
+        {
+            get
+            {
+                double sum = 0;
+                for (int i = 0; i < a.GetLength(0); i++)
+                    for (int j = 0; j < a.GetLength(1); j++)
+                        sum += a[i, j];
+                return sum / a.GetLength(0) / a.GetLength(1);
+            }
+
+        }
+
+        /// <summary>
+        /// Расчет суммы всех элементов массива.
+        /// </summary>
+        /// <returns></returns>
+        public int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    sum += a[i, j];
+            return sum;
+        }
+
+        /// <summary>
+        /// Расчет суммы всех элементов, больше заданного порога.
+        /// </summary>
+        /// <param name="key">Порог</param>
+        /// <returns></returns>
+        public int SumGreaterThen(int key)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < a.GetLength(1); j++)
+                    if (a[i, j] > key) sum += a[i, j];
+            return sum;
+        }
+        // Метод, который возвращает массив в виде строки
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                    str.AppendFormat("{0, 7}", a[i,j]);//
+                str.Append("\n"); // Переход на новую строчку
+
+            }
+            return str.ToString();
+        }
+    }
+}
---
+++ Lesson_4/Lesson_4/Task2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    partial class Program
+    {
+        /*  Томашевич
+         * 
+         * Реализуйте задачу 1 в виде статического класса StaticClass;
+         * а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;
+         * б) *Добавьте статический метод для считывания массива из текстового файла.
+         *  Метод должен возвращать массив целых чисел;
+         * в)**Добавьте обработку ситуации отсутствия файла на диске.
+         * */
+
+        static void Task2()
+        {
+            Random rnd = new Random();
+
+            /********** в этом блоке создается массив и записывается в файл **************/
+            int[] mas = new int[20];
+            Console.WriteLine("\nМассив содержит:");
+            for (int i = 0; i < mas.Length; Console.Write(mas[i++] + " "))
+            {
+                mas[i] = rnd.Next(-10000, 10000);
+            }
+
+            /********* следующий блок используется при считывании массива из файла**********/
+
+            //int[] mas = StaticTask1.ReadArrayFromFile("array.txt");
+            //if (mas == null) return;
+            //Console.WriteLine("\nМассив содержит:");
+            //for (int i = 0; i < mas.Length; Console.Write(mas[i++] + " ")) { }
+
+            StaticTask1.StaticTask1Method(mas);
+        }
+
+        static class StaticTask1
+        {
+            static public void StaticTask1Method(int[] mas)
+            {
+                Console.Write("\nКоличество пар: ");
+
+                int num = 0;
+                for (int i = 0; i < mas.Length - 1; i++)
+                {
+                    if ((mas[i] % 3 == 0) ^ (mas[i + 1] % 3 == 0)) num++;
+                }
+                Console.WriteLine(num);
+
+                //  запись массива в файл, чтоб можно было воспользоваться методом ReadArrayFromFile;
+                // можно закомментировать, если изначально массив получается путем считывания из файла,
+                //  в противном случае в файл просто перезапишутся те же значения
+
+                FileStream fstr = new FileStream("array.txt", FileMode.OpenOrCreate);
+                BinaryWriter bw = new BinaryWriter(fstr); // тут есть метод для записи Int32 (удобней чем StreaWriter)
+
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    bw.Write(mas[i]);
+                }
+                bw.Close();
+            }
+
+            // использовать только после создания файла (см. начало метода Task2())
+            static public int[] ReadArrayFromFile(string fileName)
+            {
+                int[] mas = null;
+                try
+                {
+                    using (FileStream fstr = new FileStream(fileName, FileMode.Open))
+                    {
+                        mas = new int[20];
+                        using (BinaryReader br = new BinaryReader(fstr)) //тут есть метод для считывания Int32 (удобней StreamReader)
+                        {
+                            for (int i = 0; br.BaseStream.Position < br.BaseStream.Length; i++) mas[i] = br.ReadInt32();
+                        }
+                    }
+                }
+                catch(FileNotFoundException)
+                {
+                    Console.WriteLine("Файл не найден");
+                }
+                
+                return mas;
+            }
+        }
+    }
+}
