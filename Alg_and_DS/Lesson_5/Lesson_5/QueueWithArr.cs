85e0e18791726a3090360e2b9fc215cfdb2ca751
DaniilTom <DaniilTom@users.noreply.github.com>
2019-01-26 22:11:10 +0300

    Lesson_5 task 1 4 6

---
+++ Alg_and_DS/Lesson_5/Lesson_5/QueueWithArr.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    /// <summary>
+    /// Очередь фиксированного размера 5 элементов, реализованная через массив
+    /// </summary>
+    class QueueWithArr
+    {
+        /// <summary>
+        /// Кольцевой массив
+        /// </summary>
+        int[] buf;
+
+        int size = 5;
+        int current_size = 0;
+        int first, last;
+
+        public QueueWithArr()
+        {
+            first = -1; //пустая
+            last = -1;
+            buf = new int[size];
+        }
+
+        public void Enqueue(int _data)
+        {
+            if (current_size == 0)
+            {
+                buf[0] = _data;
+                last = 0;
+                first = 0;
+                current_size++;
+                return;
+            }
+
+            last++; // сразу увеличим last на один и посмотрим на сложившуюся ситуацию
+
+            if (last == size) //если индекс last выходит за границы массива
+            {
+                if (first == 0) { last--; return; } //а индекс first указывает на начало массива => очередь заполнена, ничего не делать
+                else { last = 0; buf[last] = _data; }
+            }
+            else if (last != first) buf[last] = _data;
+            else { last--; return; } // очередь заполнена, ничего не делать
+
+            current_size++;
+        }
+
+        public int Dequeue()
+        {
+            if (current_size == 0) return -1;
+
+            int res = buf[first];
+
+            first++;
+            current_size--;
+
+            if (current_size == 0) first = last = 0;
+
+            if (first == size)
+            {
+                first = 0;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Alg_and_DS/Lesson_5/Lesson_5/SinglyLinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class SinglyLinkedList
+    {
+        // простейший односвязный список только для задачи 4.
+
+        public SinglyLinkedList() { }
+
+        Node head;
+        Node current;
+
+        public int num_elements;
+        
+        /// <summary>
+        /// Представляет узел односвязного списка, который содержит значение типа int и ссылку на следующий узел
+        /// </summary>
+        class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int _data)
+            {
+                next = null;
+                data = _data;
+            }
+        }
+
+        /// <summary>
+        /// Добавляет новый узел
+        /// </summary>
+        /// <param name="data"></param>
+        public void Add(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+                current = head;
+                num_elements++;
+                return;
+            }
+
+            Node node = head;
+            while (node.next != null) node = node.next;
+
+            node.next = new Node(data);
+            num_elements++;
+        }
+
+        /// <summary>
+        /// Возвращает значение текущего узла
+        /// </summary>
+        /// <returns></returns>
+        public int GetCurrentValue()
+        {
+            int res = current.data;
+            if (current.next != null) current = current.next;
+            else Reset();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Сбрасывает указатель на текущий узел current
+        /// </summary>
+        public void Reset()
+        {
+            current = head;
+        }
+    }
+}
