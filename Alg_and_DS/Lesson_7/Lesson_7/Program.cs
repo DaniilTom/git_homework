d1eb1218ee7d5adc0da3bf1c479cbfc4fe2a611d
DaniilTom <DaniilTom@users.noreply.github.com>
2019-02-03 12:09:53 +0300

    Lesson_7 complete

---
+++ Alg_and_DS/Lesson_7/Lesson_7/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    /*
+     * Томашевич
+     * 
+     * 1. Написать рекурсивную функцию обхода графа в глубину.
+     * 2. Написать функцию обхода графа в глубину.
+     * 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // матрица смежности графа
+            int[,] graph = new int[8, 8] {
+                { 0, 1, 1, 1, 0, 0, 0, 0 },
+                { 1, 0, 1, 0, 0, 1, 0, 1 },
+                { 1, 1, 0, 1, 1, 0, 0, 1 },
+                { 1, 0, 1, 0, 1, 0, 0, 0 },
+                { 0, 0, 1, 1, 0, 0, 1, 1 },
+                { 0, 1, 0, 0, 0, 0, 1, 1 },
+                { 0, 0, 0, 0, 1, 1, 0, 1 },
+                { 0, 1, 1, 0, 1, 1, 1, 0 }
+                };
+
+            // массив цветов: 0 - не обнаружен, 1 - обнаружен, 2 - обработан 
+            int[] color = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            Console.WriteLine("Обход в ширину:");
+            BFS(graph, 5, color);
+
+            Console.WriteLine("\nОбход в глубину:");
+            color = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 }; // обнудение цветов
+            DFS(graph, 5, color);
+
+            Console.ReadLine();
+        }
+
+        static QueueWithList queue = new QueueWithList();
+
+        /// <summary>
+        /// В глубину
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="start"></param>
+        /// <param name="color"></param>
+        static void DFS(int[,] graph, int start, int[] color)
+        {
+            Console.WriteLine("node : " + start);
+            color[start] = 1;
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (graph[start, i] == 1 && color[i] == 0)
+                {
+                    
+                    DFS(graph, i, color);
+                }
+            }
+        }
+
+        /// <summary>
+        /// В ширину
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="start"></param>
+        /// <param name="color"></param>
+        static void BFS(int[,] graph, int start, int[] color)
+        {
+            queue.Enqueue(start);
+
+            while(queue.Length != 0)
+            {
+                start = queue.Dequeue();
+                Console.WriteLine("node : " + start);
+                color[start] = 2;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if (graph[start, i] == 1 && color[i] == 0)
+                    {
+                        color[i] = 1;
+                        queue.Enqueue(i);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Alg_and_DS/Lesson_7/Lesson_7/QueueWithList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    class QueueWithList
+    {
+        SinglyLinkedListForQue buf;
+
+        public QueueWithList()
+        {
+            buf = new SinglyLinkedListForQue();
+        }
+
+        public void Enqueue(int _d)
+        {
+            buf.Enqueue(_d);
+        }
+
+        public int Dequeue()
+        {
+            return buf.Dequeue();
+        }
+
+        public int Length { get { return buf.num_elements; } }
+    }
+
+    /// <summary>
+    /// Список из прошлого задания немного переделанный для очереди
+    /// </summary>
+    class SinglyLinkedListForQue
+    {
+        // простейший односвязный список только для задачи 4.
+
+        public SinglyLinkedListForQue() { }
+
+        Node head;
+        Node last;
+
+        public int num_elements;
+
+        /// <summary>
+        /// Представляет узел односвязного списка, который содержит значение типа int и ссылку на следующий узел
+        /// </summary>
+        class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int _data)
+            {
+                next = null;
+                data = _data;
+            }
+        }
+
+        /// <summary>
+        /// Добавляет новый узел
+        /// </summary>
+        /// <param name="data"></param>
+        public void Enqueue(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+                last = head;
+                num_elements++;
+                return;
+            }
+
+            last.next = new Node(data);
+            last = last.next;
+            num_elements++;
+        }
+
+        /// <summary>
+        /// Возвращает значение текущего узла
+        /// </summary>
+        /// <returns></returns>
+        public int Dequeue()
+        {
+            if(num_elements == 1)
+            {
+                int res1 = head.data;
+                head = null;
+                last = null;
+                num_elements--;
+                return res1;
+            }
+
+            if (num_elements == 0) return -1; //очередь пуста, просто вернем минус 1
+
+            int res = head.data;
+            num_elements--;
+
+            head = head.next;
+
+
+            return res;
+        }
+    }
+}
