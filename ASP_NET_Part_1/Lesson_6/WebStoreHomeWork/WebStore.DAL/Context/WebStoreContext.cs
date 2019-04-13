1069bc62abdfbc788553510f98c120c3ed6f539b
DaniilTom <DaniilTom@users.noreply.github.com>
2019-04-13 14:26:59 +0300

    Lesson_6 created

---
+++ ASP_NET_Part_1/Lesson_6/WebStoreHomeWork/WebStore.DAL/Context/WebStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Implementations;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreContext : DbContext
+    {
+        public DbSet<Microcontroller> Microcontrollers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<MCDescription> MCDescriptions { get; set; }
+
+        public WebStoreContext(DbContextOptions<WebStoreContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<MCDescription.MCDescriptionConfiguration>(new MCDescription.MCDescriptionConfiguration());
+            modelBuilder.ApplyConfiguration<MCDescription>(new MCDescription.MCDescriptionConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ASP_NET_Part_1/Lesson_6/WebStoreHomeWork/WebStore/Infrastructure/Interfaces/IServiceCategoryData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Implementations;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    public interface IServiceCategoryData
+    {
+        IEnumerable<Category> GetCategories();
+    }
+}
