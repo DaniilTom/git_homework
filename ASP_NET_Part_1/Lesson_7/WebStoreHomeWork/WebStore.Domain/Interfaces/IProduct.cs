144bc19ca44e7555cd924664f1cb0883027abe57
DaniilTom <DaniilTom@users.noreply.github.com>
2019-04-18 22:47:03 +0300

    created

---
+++ ASP_NET_Part_1/Lesson_7/WebStoreHomeWork/WebStore.Domain/Interfaces/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Interfaces
+{
+    public interface IProduct
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        int CategoryId { get; set; }
+        string ImageUrl { get; set; }
+    }
+}
---
+++ ASP_NET_Part_1/Lesson_7/WebStoreHomeWork/WebStore/Infrastructure/Implementations/SqlProductData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Implementations;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    public class SqlProductData : IServiceAllData
+    {
+        private readonly WebStoreContext _db;
+
+        public SqlProductData(WebStoreContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Microcontroller> Microcontrollers => _db.Microcontrollers;
+
+        public IEnumerable<MCDescription> DetailedDescription => _db.MCDescriptions;
+
+        public IEnumerable<Category> GetCategories() => _db.Categories;
+
+        public void AddNew(Microcontroller employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Microcontroller GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
