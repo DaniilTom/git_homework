de5d1ba05c277c1decd8f13c1006c16677fe283e
DaniilTom <DaniilTom@users.noreply.github.com>
2019-04-21 08:47:43 +0300

    Lesson_8 created

---
+++ ASP_NET_Part_1/Lesson_8/WebStoreHomeWork/WebStore/Components/MicrocontrollerViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModel;
+
+namespace WebStore.Components
+{
+    public class MicrocontrollerViewComponent : ViewComponent
+    {
+        private readonly IServiceMicrocontrollerData _MicrocontrollerData;
+
+        public MicrocontrollerViewComponent(IServiceMicrocontrollerData MicrocontrollerData)
+        {
+            _MicrocontrollerData = MicrocontrollerData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(GetProducts());
+        }
+
+        private IEnumerable<MicrocontrollerViewModel> GetProducts()
+        {
+            var mc = _MicrocontrollerData.Microcontrollers;
+            var desc = _MicrocontrollerData.DetailedDescription;
+
+            return from m in mc
+                   from d in desc
+                   where m.Id == d.ProductId
+                   select new MicrocontrollerViewModel {Microcontroller = m, DetailedDescription = d };
+        }
+    }
+}
