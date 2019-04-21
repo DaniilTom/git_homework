a98f0866dbd20ae3ee9403eee61e776b9730badc
DaniilTom <DaniilTom@users.noreply.github.com>
2019-04-21 18:05:47 +0300

    Img Uploader added

--- ASP_NET_Part_1/Lesson_8/WebStoreHomeWork/WebStore/Areas/Admin/Controllers/ToolsController.cs
+++ ASP_NET_Part_1/Lesson_8/WebStoreHomeWork/WebStore/Areas/Admin/Controllers/ToolsController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using WebStore.DAL.Context;
 using WebStore.Areas.Admin.ViewModels;
 using WebStore.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace WebStore.Areas.Admin.Controllers
 {
@@ -47,6 +50,35 @@ namespace WebStore.Areas.Admin.Controllers
         {
             _db.Microcontrollers.Add(new Domain.Implementations.Microcontroller { Name = name, Price = price, CategoryId = int.Parse(catId)});
 
+            _db.SaveChanges();
+
+            return RedirectToAction("StoreHouse");
+        }
+
+        [HttpPost]
+        public IActionResult AddDescription(int id, string desc)
+        {
+            string[] format_desc = desc.Split("\r\n", StringSplitOptions.RemoveEmptyEntries) ;
+
+            _db.MCDescriptions.Add(new Domain.Implementations.MCDescription
+            {
+                ProductId = id,
+                DetailedDesriptionList = format_desc
+            });
+
+            _db.SaveChanges();
+            return RedirectToAction("StoreHouse");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImgAsync(IFormFile file, [FromServices] IHostingEnvironment _appEnvironment, int id)
+        {
+            string path = _appEnvironment.WebRootPath + "/img/" + file.FileName;
+            using (var fileStream = new FileStream(path, FileMode.CreateNew))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            _db.Microcontrollers.First(m => m.Id == id).ImageUrl = "/img/" + file.FileName;
             _db.SaveChanges();
 
             return RedirectToAction("StoreHouse");
