3407d17f91c03cc47a5378fa9f9c023ea075520a
DaniilTom <DaniilTom@users.noreply.github.com>
2019-05-10 10:40:24 +0300

    Lesson_2 created

---
+++ ASP_NET_Part_2/Lesson_2/WebStoreHomeWork/UI/WebStore/Components/ProductBaseViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Interfaces.Services;
+using WebStore.ViewModel;
+
+namespace WebStore.Components
+{
+    public class ProductBaseViewComponent : ViewComponent
+    {
+        private readonly IServiceProductData _MicrocontrollerData;
+
+        public ProductBaseViewComponent(IServiceProductData ProductData)
+        {
+            _MicrocontrollerData = ProductData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(GetProducts());
+        }
+
+        private IEnumerable<MicrocontrollerViewModel> GetProducts()
+        {
+            var mc = _MicrocontrollerData.Products;
+            var desc = _MicrocontrollerData.DetailedDescription;
+
+            return from m in mc
+                   from d in desc
+                   where m.Id == d.ProductId
+                   select new MicrocontrollerViewModel {ProductBase = m, DetailedDescription = d };
+        }
+    }
+}
---
+++ ASP_NET_Part_2/Lesson_2/WebStoreHomeWork/UI/WebStore/Infrastructure/Implementations/SqlProductData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Implementations;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    public class SqlProductData : IServiceAllData
+    {
+        private readonly WebStoreContext _db;
+
+        public SqlProductData(WebStoreContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<ProductBase> Products => _db.Products;
+
+        public IEnumerable<MCDescription> DetailedDescription => _db.MCDescriptions;
+
+        public IEnumerable<Category> GetCategories() => _db.Categories;
+
+        public IEnumerable<Order> Orders => _db.Orders.Include(o => o.Items).ThenInclude(i => i.Product).AsEnumerable();
+
+        public IEnumerable<OrderItem> OrderItems => _db.OrderItems;
+
+        public void AddNewProduct(ProductBase prod)
+        {
+            _db.Products.Add(prod);
+            _db.SaveChanges();
+        }
+
+        public void AddNewDescription(MCDescription description)
+        {
+            _db.MCDescriptions.Add(description);
+            _db.SaveChanges();
+        }
+
+        public void AddNewOrder(Order order)
+        {
+            _db.Orders.Add(order);
+            _db.SaveChanges();
+        }
+
+        public void AddNewOrderItem(OrderItem orderItem)
+        {
+            _db.OrderItems.Add(orderItem);
+            _db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductBase GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
