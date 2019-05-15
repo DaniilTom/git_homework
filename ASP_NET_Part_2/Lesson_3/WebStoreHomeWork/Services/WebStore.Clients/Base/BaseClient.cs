2e66556e4fa0dfee71ec9be29420559228ec8769
DaniilTom <DaniilTom@users.noreply.github.com>
2019-05-15 16:08:39 +0300

    Lesson_3 created

---
+++ ASP_NET_Part_2/Lesson_3/WebStoreHomeWork/Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected readonly HttpClient _Client;
+
+        protected string ServiceAddress { get; }
+
+        protected BaseClient(IConfiguration configuration, string ServiceAdress)
+        {
+            this.ServiceAddress = ServiceAdress;
+
+            _Client = new HttpClient
+            {
+                BaseAddress = new Uri(configuration["ClientAddress"])
+            };
+
+            _Client.DefaultRequestHeaders.Accept.Clear();
+            _Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
