9b7a332c819328e8213a7f3e38ce18e94db52037
DaniilTom <DaniilTom@users.noreply.github.com>
2019-06-01 17:20:27 +0300

    Lesson_7 created

---
+++ ASP_NET_Part_2/Lesson_7/WebStoreHomeWork/UI/WebStore/controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmartBreadcrumbs.Attributes;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.controllers
+{
+    
+    //[Controller]
+    public class HomeController : Controller
+    {
+        //private readonly IServiceProductData _MicrocontrollerData;
+
+        public HomeController(/*IServiceProductData ProductData*/)
+        {
+            //_MicrocontrollerData = ProductData;
+        }
+
+        [DefaultBreadcrumb("Home")]
+        public IActionResult Index()
+        {
+            //return Content("Hello World!!");
+            return View();
+        }
+
+        [Breadcrumb("Contact")]
+        public IActionResult Contact([FromServices] ILogger<HomeController> log)
+        {
+            log.LogWarning("Контакты");
+            return View();
+        }
+
+        [Breadcrumb("Catalog", FromAction = "Contact")] //FromAction просто так, посмотреть
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+    }
+}
